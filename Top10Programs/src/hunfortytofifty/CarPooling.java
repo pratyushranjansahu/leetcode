33fe144afdc463dfba9c08dc95e317fe3b099a45
pratyushranjansahu <sahu.ranjan.pratyush@gmail.com>
2022-04-25 13:37:46 +0530

    Car Pooling

---
+++ Top10Programs/src/hunfortytofifty/CarPooling.java
@@ -0,0 +1,36 @@
+package hunfortytofifty;
+//https://www.youtube.com/watch?v=nO95uYKB-lo
+//Leetcode : 1094
+public class CarPooling {
+    public static void main(String[] args) {
+        //int[][] trips = {{1,5,2},{4,8,1},{11,15,3}}; int capacity = 3;(youtube example .som changed in index)
+        int[][] trips = {{2,1,5},{3,3,7}}; int capacity = 4;
+       // int[][] trips = {{9,0,1},{3,3,7}}; int capacity = 4;
+
+
+        System.out.println(carPooling(trips, capacity));
+    }
+
+
+
+    public static boolean carPooling(int[][] trips, int capacity) {
+        int lastDrawnLocation = -1;
+        for(int[] trip : trips){
+            lastDrawnLocation = Math.max(lastDrawnLocation,trip[2]);
+        }
+        int[] highHay = new int[lastDrawnLocation+1];
+        for (int[] trip : trips){
+            highHay[trip[1]] += trip[0];
+            highHay[trip[2]] -= trip[0];
+        }
+
+        for (int i = 1; i <= lastDrawnLocation; i++){
+            if(highHay[i-1]> capacity)
+                return false;
+            highHay[i] += highHay[i-1];
+            if(highHay[i] > capacity)
+                return false;
+        }
+        return true;
+    }
+}
