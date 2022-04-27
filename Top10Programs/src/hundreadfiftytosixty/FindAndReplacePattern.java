cee214c1e25a131deb6c0d5945ffbed256e035dd
pratyushranjansahu <sahu.ranjan.pratyush@gmail.com>
2022-04-27 11:20:27 +0530

    Find and replace pattern

---
+++ Top10Programs/src/hundreadfiftytosixty/FindAndReplacePattern.java
@@ -0,0 +1,46 @@
+package hundreadfiftytosixty;
+
+import java.util.*;
+/*
+    https://leetcode.com/problems/find-and-replace-pattern/
+    https://www.youtube.com/watch?v=4Ze_vLq5tPQ
+ */
+public class FindAndReplacePattern {
+    public static void main(String[] args) {
+        String[] words = {"abc","deq","mee","aqq","dkd","ccc"};
+        String pattern = "abb";
+        System.out.println(findAndReplacePattern(words, pattern));//Output: ["mee","aqq"]
+    }
+    public static List<String> findAndReplacePattern(String[] words, String pattern) {
+        List<String> res = new ArrayList<>();
+        for (String word : words){
+            if(isMatching(word, pattern)){
+                res.add(word);
+            }
+        }
+        return res;
+    }
+    private static boolean isMatching(String word, String pattern){
+        Map<Character, Character> map = new HashMap<Character, Character>();
+        Set<Character> set = new HashSet<Character>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char wch = word.charAt(i);
+            char pch = pattern.charAt(i);
+
+            if (map.containsKey(pch) == true) {
+                if (map.get(pch) != wch){
+                    return false;
+                }
+            } else {
+                if (set.contains(wch)) {
+                    return false;
+                }
+
+                map.put(pch, wch);
+                set.add(wch);
+            }
+        }
+        return true;
+    }
+}
