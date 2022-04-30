a478a239d97a0037ac4590ae84faae83db73d818
pratyushranjansahu <sahu.ranjan.pratyush@gmail.com>
2022-04-30 07:27:39 +0530

    Range Addition

---
+++ Top10Programs/src/hundreadfiftytosixty/RangeAddition.java
@@ -0,0 +1,36 @@
+package hundreadfiftytosixty;
+/*
+    https://www.pepcoding.com/resources/data-structures-and-algorithms-in-java-levelup/arrays-and-strings/range_addition/ojquestion
+    https://www.youtube.com/watch?v=fBT0VKkqvtY
+ */
+public class RangeAddition {
+    public static void main(String[] args) {
+        int length = 5;
+        int[][] updates = {
+                {1,  3,  2},
+                {2,  4,  3},
+                {0,  2, -2}
+        };
+        int[] res = getModifiedArray(length, updates);
+
+    }
+    private static int[] getModifiedArray(int length, int[][] queries) {
+        int[] res = new int[length];
+        for (int q = 0; q < queries.length; q++) {
+             int start = queries[q][0];
+             int end = queries[q][1];
+             int inc = queries[q][2];
+
+             res[start] += inc;
+             if (end+1 < length) {
+                 res[end+1] -= inc;
+             }
+        }
+        int sum = 0;
+        for (int i = 0; i < length; i++) {
+            sum += res[i];
+            res[i] = sum;
+        }
+        return  res;
+    }
+}
