7a75e2296937a8a23fe54761ef0cd0da318aa739
pratyushranjansahu <sahu.ranjan.pratyush@gmail.com>
2022-05-10 21:43:23 +0530

    Sliding Window Maximum

---
+++ Top10Programs/src/hunseventytoeighty/SlidingWindowMaximum.java
@@ -0,0 +1,41 @@
+package hunseventytoeighty;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+/*
+ * https://leetcode.com/problems/sliding-window-maximum/
+ * https://www.youtube.com/watch?v=tCVOQX3lWeI
+ */
+public class SlidingWindowMaximum {
+    public static void main(String[] args) {
+        int[] arr = {1,3,-1,-3,5,3,6,7};
+        int k = 3;
+        maxSlidingWindowLeetcode(arr, k);//[3,3,5,5,6,7]
+    }
+    public static int[] maxSlidingWindowLeetcode(int[] a, int k) {
+        List<Integer> res = new ArrayList();
+        Stack<Integer> stack =new Stack();
+        int[] nge = new int [a.length];
+        for (int i = a.length-1; i >= 0; i--) {
+            if (!stack.isEmpty()) {
+                while (!stack.isEmpty() && a[i] >= a[stack.peek()]) {
+                    stack.pop();
+                }
+
+            }
+            nge[i] = stack.isEmpty() ? a.length : stack.peek();
+            stack.push(i);
+        }
+        for (int i = 0; i <= a.length-k; i++) {
+            int j = i;
+            while (nge[j] < i+k) {
+                j = nge[j];
+
+            }
+            res.add(a[j]);
+        }
+        int[] resArray = res.stream().mapToInt(i->i).toArray();
+        return resArray;
+    }
+}
