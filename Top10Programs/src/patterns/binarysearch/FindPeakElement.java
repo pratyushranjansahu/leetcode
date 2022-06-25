ffc0b4ef5acc67f412cec13888a0de77701c9c44
pratyushranjansahu <sahu.ranjan.pratyush@gmail.com>
2022-06-25 17:33:36 +0530

    find peak element

---
+++ Top10Programs/src/patterns/binarysearch/FindPeakElement.java
@@ -0,0 +1,27 @@
+package patterns.binarysearch;
+/*
+    https://leetcode.com/problems/find-peak-element/
+    https://www.youtube.com/watch?v=uZkyxzn3RzA
+ */
+public class FindPeakElement {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1};
+        System.out.println(findPeakElement(nums)); //2
+    }
+    public static int findPeakElement(int[] nums) {
+        if(nums.length==1)return 0;
+
+        int low = 0 , high = nums.length-1;
+        while(low <= high){
+            int mid = (low+high)/2;
+            if((mid==0 || nums[mid]>nums[mid-1]) && (mid==nums.length-1 || nums[mid]>nums[mid+1]))return mid;
+
+            if(nums[mid]>nums[mid+1]){
+                high = mid;
+            }else{
+                low = mid+1;
+            }
+        }
+        return high;
+    }
+}
