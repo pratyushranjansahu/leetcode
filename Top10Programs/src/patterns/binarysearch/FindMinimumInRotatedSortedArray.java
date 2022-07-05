e4da0be6c176028bc5d9c784f84fadace6f8c87f
pratyushranjansahu <sahu.ranjan.pratyush@gmail.com>
2022-07-06 04:25:26 +0530

    binary search format changed

--- Top10Programs/src/patterns/binarysearch/FindMinimumInRotatedSortedArray.java
+++ Top10Programs/src/patterns/binarysearch/FindMinimumInRotatedSortedArray.java
@@ -16,7 +16,7 @@ public class FindMinimumInRotatedSortedArray {
         }
 
         while (lo <= hi) {
-            int mid = (lo + hi) / 2;
+            int mid = lo + (hi - lo) / 2;
             if (nums[mid] > nums[mid + 1]) {
                 return nums[mid + 1];
             }else if (nums[mid] < nums[mid - 1]) {
