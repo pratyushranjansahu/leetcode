b60a8f265c1becf0dee1188dd8092cfb5770687a
pratyushranjansahu <sahu.ranjan.pratyush@gmail.com>
2022-07-06 04:27:58 +0530

    changes in binary search format

--- Top10Programs/src/patterns/binarysearch/FindPeakElement.java
+++ Top10Programs/src/patterns/binarysearch/FindPeakElement.java
@@ -13,7 +13,7 @@ public class FindPeakElement {
 
         int low = 0 , high = nums.length-1;
         while(low <= high){
-            int mid = (low+high)/2;
+            int mid = low + (high - low)/2;
             if((mid==0 || nums[mid]>nums[mid-1]) && (mid==nums.length-1 || nums[mid]>nums[mid+1]))return mid;
 
             if(nums[mid]>nums[mid+1]){
