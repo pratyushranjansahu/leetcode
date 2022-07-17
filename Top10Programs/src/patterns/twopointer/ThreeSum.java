4df37a0a7c895616c16b22af771353ff00b28ba9
pratyushranjansahu <sahu.ranjan.pratyush@gmail.com>
2022-07-17 11:26:05 +0530

    code modified

--- Top10Programs/src/patterns/twopointer/ThreeSum.java
+++ Top10Programs/src/patterns/twopointer/ThreeSum.java
@@ -5,47 +5,51 @@ import java.util.Arrays;
 import java.util.List;
 /*
 	https://leetcode.com/problems/3sum/
-	https://www.youtube.com/watch?v=Ca7k53qcTic&t=1156s
+	https://www.youtube.com/watch?v=E7Ie6OlQgN4
  */
 public class ThreeSum {
-
+	static List<List<Integer>> res = new ArrayList();
 	public static void main(String[] args) {
 		//int[] arr = {-1, 0, 1, 2, -1, -4};
 		int[] arr = {3,0,-2,-1,1,2};
-		System.out.println(threeSum(arr));
+		System.out.println(threeSum(arr));//[[-2, -1, 3], [-2, 0, 2], [-1, 0, 1]]
 	}
 
-	private static List<List<Integer>> threeSum(int[] arr) {
-		List<List<Integer>> result = new ArrayList();
-		Arrays.sort(arr);
-		for (int i = 0; i < arr.length-2; i++) {
-			//if(arr[i]!=arr[i+1]) {
-			 if(i == 0 || arr[i] != arr[i-1]){
-				int j=i+1;
-				int k=arr.length-1;
-				while(j<k) {
-					int sum=arr[i]+arr[j]+arr[k];
-					if(sum==0) {
-						List<Integer> list=new ArrayList<>();
-						list.add(arr[i]);
-						list.add(arr[j]);
-						list.add(arr[k]);
-						result.add(list);
-						while(j<k && arr[j]==arr[j+1]) j++;
-						while(j<k && arr[k]==arr[k-1]) k--;
-						j++;
-						k--;
-					}else if(sum<0)
-						j++;
-					else
-						k--;
-					
-					
-				}
+
+	public static List<List<Integer>> threeSum(int[] nums) {
+		Arrays.sort(nums);
+		for(int i=0;i<nums.length;i++){
+			if(i==0 || nums[i-1]!=nums[i]){
+				twoSumSorted(i+1,nums.length-1,nums,0-nums[i]);
 			}
-			
 		}
-		return result;
+		return res;
+	}
+
+
+	static void  twoSumSorted(int i,int j,int[] nums,int target){
+		int a1 = nums[i-1];
+		while(i<j){ //search space
+			if(nums[i]+nums[j]>target){
+				j--;
+			}else if(nums[i]+nums[j]<target){
+				i++;
+			}else{
+				List<Integer> list = new ArrayList();
+				list.add(a1);list.add(nums[i]);list.add(nums[j]);
+
+				res.add(list);
+
+
+
+				//duplicate b
+				while(i<j && nums[i]==nums[i+1])i++;
+				//duplicate c
+				while(i<j && nums[j]==nums[j-1])j--;
+
+				i++;j--;
+			}
+		}
 	}
 
 }
