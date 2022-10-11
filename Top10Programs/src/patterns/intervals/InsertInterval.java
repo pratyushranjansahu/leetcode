1ee907675ada7ca7761cd3aa413be9306dce2987
pratyushranjansahu <sahu.ranjan.pratyush@gmail.com>
2022-10-11 21:12:21 +0530

    code  modified

--- Top10Programs/src/patterns/intervals/InsertInterval.java
+++ Top10Programs/src/patterns/intervals/InsertInterval.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 /*
     https://leetcode.com/problems/insert-interval/
-    https://www.youtube.com/watch?v=FuLfL_WhUHI
+    https://www.youtube.com/watch?v=dxbRB6gWCqg
  */
 public class InsertInterval {
     public static void main(String[] args) {
@@ -12,19 +12,34 @@ public class InsertInterval {
         int[] newInterval = {2,5};
         insert(intervals, newInterval);//[[1,5],[6,9]]
     }
+
     public static int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> result = new ArrayList();
         int i = 0, n = intervals.length;
-        while(i < n && intervals[i][1] < newInterval[0]) result.add(intervals[i++]);
-
-        int[] mI = newInterval;
-        while(i < n && intervals[i][0] <= newInterval[1]){
-            mI[0] = Math.min(mI[0], intervals[i][0]);
-            mI[1] = Math.max(mI[1], intervals[i++][1]);
+        while (i < n){
+            if (intervals[i][0] < newInterval[0]){
+                result.add(intervals[i]);
+                i++;
+            }else {
+                break;
+            }
         }
-        result.add(mI);
+        if (result.size() == 0 || newInterval[0] > result.get(result.size() - 1)[1]){
+            result.add(newInterval);
+        }else {
+            int[] lastInterval = result.get(result.size() - 1);
+            lastInterval[1] = Math.max(lastInterval[1],newInterval[1]);
+        }
 
-        while(i < n) result.add(intervals[i++]);
+        while (i < n){
+            int[] lastInterval = result.get(result.size() - 1);
+            if (lastInterval[1] >= intervals[i][0]){
+                lastInterval[1] = Math.max(lastInterval[1],intervals[i][1]);
+            }else {
+                result.add(intervals[i]);
+            }
+            i++;
+        }
 
         return result.toArray(new int[result.size()][2]);
     }
