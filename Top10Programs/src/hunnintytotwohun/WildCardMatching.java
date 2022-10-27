fc2a92fd8f0e54bc794c4c1c2a13c645df06ca80
pratyushranjansahu <sahu.ranjan.pratyush@gmail.com>
2022-10-27 19:19:10 +0530

    code refractoring

--- Top10Programs/src/hunnintytotwohun/WildCardMatching.java
+++ Top10Programs/src/hunnintytotwohun/WildCardMatching.java
@@ -12,13 +12,13 @@ public class WildCardMatching {
         int n = p.length();
         int m = s.length();
         boolean[][] dp = new boolean[n+1][m+1];
-        for (int i = dp.length-1; i >= 0; i--) {
-            for (int j = dp[0].length-1; j >= 0; j--) {
-                if (i == dp.length-1 && j == dp[0].length-1) {
+        for (int i = n; i >= 0; i--) {
+            for (int j = m; j >= 0; j--) {
+                if (i == n && j == m) {
                     dp[i][j] = true;
-                }else if (i == dp.length-1) {
+                }else if (i == n) {
                     dp[i][j] = false;
-                }else if (j == dp[0].length-1) {
+                }else if (j == m) {
                     if (p.charAt(i) == '*') {
                         dp[i][j] = dp[i+1][j];
                     }else {
