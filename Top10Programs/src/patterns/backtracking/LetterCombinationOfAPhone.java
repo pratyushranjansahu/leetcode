59a1c2677f7a9f33e763e8cef5898e8b579271f9
pratyushranjansahu <sahu.ranjan.pratyush@gmail.com>
2023-04-11 03:51:50 +0530

    implemented using Backtracking.

--- Top10Programs/src/patterns/backtracking/LetterCombinationOfAPhone.java
+++ Top10Programs/src/patterns/backtracking/LetterCombinationOfAPhone.java
@@ -3,7 +3,7 @@ package patterns.backtracking;
 import java.util.*;
 /*
 	https://leetcode.com/problems/letter-combinations-of-a-phone-number/
-	https://www.youtube.com/watch?v=8orpUBZZ9l0&t=759s
+	https://www.youtube.com/watch?v=GqE96uMPOLI
  */
 public class LetterCombinationOfAPhone {
 
@@ -11,36 +11,35 @@ public class LetterCombinationOfAPhone {
 		System.out.println(letterCombinations("23"));//[ad, ae, af, bd, be, bf, cd, ce, cf]
 	}
 	
-	 public static  List<String> letterCombinations(String digits) {
-	      
-	        List<String> result = new ArrayList();
-	        if(digits.length()==0)return result;
-	        
-	        HashMap<String,String> map = new HashMap();
-	        map.put("2","abc"); map.put("3","def"); map.put("4","ghi");
-	        map.put("5","jkl"); map.put("6","mno"); map.put("7","pqrs");
-	        map.put("8","tuv"); map.put("9","wxyz");
-	        
-	        Deque<String> deque = new ArrayDeque();
-	        deque.add("");   
-	        
-	        for(int i=0;i<digits.length();i++){//2   3      4
-	        	//System.out.println(i+" "+(i+1));
-	            String d = digits.substring(i,i+1); //2   3
-	            String fromMap = map.get(d); //"abc"   "def"
-	            int n = deque.size();
-	            for(int j=0;j<n;j++){
-	                String pull = deque.pollFirst(); //[a]        [b][c]   
-	                for(int k=0;k<fromMap.length();k++){
-	                    String temp = pull.concat(fromMap.charAt(k)+"");//[ad] [ae] [ai]
-	                    deque.add(temp);//[ad] [ae] [ai]
-	                }
-	            }
-	        }
-	        
-	        while(!deque.isEmpty()){
-	            result.add(deque.poll());
-	        }
-	        return result;
+	public static  List<String> letterCombinations(String digits) {
+
+		List<String> ans = new ArrayList<>();
+		 if(digits.length() == 0)
+			 return ans;
+
+		 HashMap<Character, String> hm = new HashMap<>();
+		 hm.put('2', "abc");
+		 hm.put('3', "def");
+		 hm.put('4', "ghi");
+		 hm.put('5', "jkl");
+		 hm.put('6', "mno");
+		 hm.put('7', "pqrs");
+		 hm.put('8', "tuv");
+		 hm.put('9', "wxyz");
+
+		 backtrack(digits, 0, hm, new StringBuilder(), ans);
+		 return ans;
 	    }
+	private static void backtrack(String digits, int i, HashMap<Character, String> hm, StringBuilder sb, List<String> ans){
+		if(i == digits.length()){
+			ans.add(sb.toString());
+			return;
+		}
+		String curr =  hm.get(digits.charAt(i));
+		for(int k =0; k <curr.length(); k++){
+			sb.append(curr.charAt(k));
+			backtrack(digits, i+1, hm,  sb, ans);
+			sb.deleteCharAt(sb.length() -1);
+		}
+	}
 }
