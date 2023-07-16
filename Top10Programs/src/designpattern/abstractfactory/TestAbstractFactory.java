22e91175371309a294badf1a4b4e52bbaec6de81
pratyushranjansahu <sahu.ranjan.pratyush@gmail.com>
2023-07-16 18:54:25 +0530

    comment added.

--- Top10Programs/src/designpattern/abstractfactory/TestAbstractFactory.java
+++ Top10Programs/src/designpattern/abstractfactory/TestAbstractFactory.java
@@ -1,5 +1,8 @@
 package designpattern.abstractfactory;
-
+/*
+    https://www.youtube.com/watch?v=7g9S371qzwM&list=PL6W8uoQQ2c61X_9e6Net0WdYZidm7zooW&index=7
+    https://darrenfinch.com/abstract-factory-pattern-explained-creational-design-patterns/
+ */
 public class TestAbstractFactory {
     public static void main(String[] args) {
         AbstractVehicleFactory vehicleFactory = VehicleFactoryProducer.createVehicleFactory("Truck");
